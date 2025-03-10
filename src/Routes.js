3ccdaa06f3f271ecfd4b571ab45e83862e6469ed
yenis <yenisleydimartinez13@gmail.com>
2025-03-10 13:51:20 -0600

    avances footer

--- src/Routes.js
+++ src/Routes.js
@@ -3,12 +3,15 @@ import { Routes, Route } from 'react-router-dom';
 import { MisDatos } from './pages/MisDatosPages/MisDatos';
 import { NavInesis } from './components/NavInesis/NavInesis';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import FooterInesis from './components/FooterInesis/FooterInesis';
 
 const AppRoutes = () => {
   return (
     <Routes>
         <Route path="/MisDatos" element={<MisDatos />} />
         <Route path="/NavInesis" element={<NavInesis />} />
+        <Route path="/FooterInesis" element={<FooterInesis />} />
+
     </Routes>
   )
 }
