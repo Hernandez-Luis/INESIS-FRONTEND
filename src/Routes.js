33f72a19c2af9224be4460473fac9b8dc314ac31
LuisPerezcx <luisperez90377@gmail.com>
2025-03-10 14:13:09 -0600

    avances lineamientos

--- src/Routes.js
+++ src/Routes.js
@@ -3,12 +3,14 @@ import { Routes, Route } from 'react-router-dom';
 import { MisDatos } from './pages/MisDatosPages/MisDatos';
 import { NavInesis } from './components/NavInesis/NavInesis';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { LIneamientos } from './pages/Lineamientos/LIneamientos';
 
 const AppRoutes = () => {
   return (
     <Routes>
         <Route path="/MisDatos" element={<MisDatos />} />
         <Route path="/NavInesis" element={<NavInesis />} />
+        <Route path="/Lineamientos" element={<LIneamientos />} />
     </Routes>
   )
 }
