8f1c7b402d172139763ec3128da097825183d422
Hernandez-Luis <luisalbertohdzrmz2003@gmail.com>
2025-06-23 18:12:48 -0600

    Limitacion por rol

--- src/components/NavInesis/NavInesis.jsx
+++ src/components/NavInesis/NavInesis.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
@@ -23,31 +23,63 @@ export const NavInesis = () => {
     navigate('/'); // Redirige al login
   };
 
+  const [usuario, setUsuario] = useState(null);
+
+  useEffect(() => {
+    const usuarioString = localStorage.getItem('usuario'); // Esto es un STRING
+    if (usuarioString) {
+      const usuarioObjeto = JSON.parse(usuarioString); // Aquí ya es un OBJETO
+      setUsuario(usuarioObjeto);
+    }
+  }, []);
+
 
   return (
     <div style={{ background: 'var(--color-gris2)', borderRadius: '12px' }} className='m-4 px-5'>
       <Navbar expand="lg">
         <Container fluid>
-          <Navbar.Brand href="/menuAlumno" className='fs-1 me-auto d-flex align-items-center'>
-            <h1 style={{ color: 'var(--color-morado1)', fontWeight: 'bold' }}>
-              INESIS
-            </h1>
-          </Navbar.Brand>
+          {usuario && usuario.rol == 1 && (
+            <Navbar.Brand href="/menuAlumno" className='fs-1 me-auto d-flex align-items-center'>
+              <h1 style={{ color: 'var(--color-morado1)', fontWeight: 'bold' }}>
+                INESIS
+              </h1>
+            </Navbar.Brand>
+          )}
+
+          {usuario && usuario.rol == 2 && (
+            <Navbar.Brand href="/menuAdministrador" className='fs-1 me-auto d-flex align-items-center'>
+              <h1 style={{ color: 'var(--color-morado1)', fontWeight: 'bold' }}>
+                INESIS
+              </h1>
+            </Navbar.Brand>
+          )}
+
+          {usuario && usuario.rol == 3 && (
+            <Navbar.Brand href="/menuRevisor" className='fs-1 me-auto d-flex align-items-center'>
+              <h1 style={{ color: 'var(--color-morado1)', fontWeight: 'bold' }}>
+                INESIS
+              </h1>
+            </Navbar.Brand>
+          )}
 
           <Navbar.Toggle aria-controls="basic-navbar-nav" style={{ backgroundColor: 'white' }} />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="ms-auto d-flex align-items-center">
-              <NavItem className='me-5 fs-5 opciones nav-item-op' style={{ color: "var(--color-morado2)" }}>
+              {usuario && usuario.rol == 1 && (<NavItem className='me-5 fs-5 opciones nav-item-op' style={{ color: "var(--color-morado2)" }}>
                 <Link to="/menuSolicitar" className="text-decoration-none text-dark">
                   <p className='mb-0' style={{ color: "var(--color-morado2)" }}>Estudio socioeconómico</p>
                 </Link>
               </NavItem>
+              )}
 
-              <NavItem className='me-5 fs-5 opciones nav-item-op'>
-                <Link to="/lineamientos" className="text-decoration-none text-dark">
-                  <p className='mb-0' style={{ color: "var(--color-morado2)" }}>Lineamientos</p>
-                </Link>
-              </NavItem>
+              {usuario && usuario.rol != 2 && (
+                <NavItem className='me-5 fs-5 opciones nav-item-op'>
+                  <Link to="/lineamientos" className="text-decoration-none text-dark">
+                    <p className='mb-0' style={{ color: "var(--color-morado2)" }}>Lineamientos</p>
+                  </Link>
+                </NavItem>
+              )}
+
 
               <NavDropdown className='me-5 no-caret custom-dropdown' title={<img src={perfilIcon} style={{ width: '40px' }} alt="Perfil" />}>
                 <NavDropdown.Item onClick={() => setShowProfileModal(true)}>Mi cuenta</NavDropdown.Item>
--- src/pages/MenuAdministrador/MenuAdministrador.jsx
+++ src/pages/MenuAdministrador/MenuAdministrador.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import MigasRecorrido from '../../components/MigasDePan/MigasRecorrido'
 import NavInesis from '../../components/NavInesis/NavInesis'
 import FooterInesis from '../../components/FooterInesis/FooterInesis'
@@ -9,13 +9,22 @@ import '../../styles/StyleMenuAdministrador/StyleMenuAdministrador.css'
 import { CardMenu } from '../MenuSolicitarBeca/components/CardMenu'
 
 export const MenuAdministrador = () => {
+     const [usuario, setUsuario] = useState(null);
+
+    useEffect(() => {
+        const usuarioString = localStorage.getItem('usuario'); // Esto es un STRING
+        if (usuarioString) {
+            const usuarioObjeto = JSON.parse(usuarioString); // Aquí ya es un OBJETO
+            setUsuario(usuarioObjeto);
+        }
+    }, []);
 
     return (
         <div>
             <NavInesis></NavInesis>
             <div className='d-flex flex-column min-vh-100'>
                 <div className='flex-grow-1 m-5 px-5 d-flex justify-content-center' style={{ color: 'var(--color-morado2)' }}>
-                    <h1>Bienvenido Luis David Jimenez Santos</h1>
+                    <h1>Bienvenido {usuario ? usuario.usuario : 'Usuario'}</h1>
                 </div>
                 <div className='container-fluid align-items-center justify-content-center text-center mb-5'>
                     <div className='row d-flex justify-content-center'>
